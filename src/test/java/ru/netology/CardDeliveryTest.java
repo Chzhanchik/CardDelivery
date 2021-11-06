8bc1f136a98dfaa0c92cd00de5fd7adcf5703cef
chzhanchik <chzhanchik@gmail.com>
2021-11-06 20:01:27 +0300

    origin

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,13 +1,23 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
     private WebDriver driver;
@@ -30,7 +40,13 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTest1() {
-        driver.get("http://localhost:9999");
-
+        open("http://localhost:9999");
+        $("[name='city']").setValue("Калининград");
+        $("[name='date']").setValue("09.11.2021");
+        $("[name='name']").setValue("Боярский Михаил");
+        $("[name='phone']").setValue("+79999999988");
+        $(withText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content");
     }
 }
