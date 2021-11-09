750224f8c2bc49980f117127aae7140d2ca09a83
chzhanchik <chzhanchik@gmail.com>
2021-11-09 22:06:43 +0300

    origin

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Web Interface Testing 
+# Web Interface Testing [![Build status](https://ci.appveyor.com/api/projects/status/r2iuvueoq82dey98?svg=true)](https://ci.appveyor.com/project/Chzhanchik/carddelivery)
--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,52 +1,37 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
+    String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
     @Test
-    void shouldTest1() {
+    public void shouldReturnResultWithCorrectlyValues() {
         open("http://localhost:9999");
-        $("[name='city']").setValue("Калининград");
-        $("[name='date']").setValue("09.11.2021");
-        $("[name='name']").setValue("Боярский Михаил");
-        $("[name='phone']").setValue("+79999999988");
-        $(withText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(".notification__content");
+        $("[data-test-id='city'] input").setValue("Калининград");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(date);
+        $("[data-test-id='name'] input").setValue("Боярский Михаил");
+        $("[data-test-id='phone'] input").setValue("+78884938474");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(exactText("Встреча успешно забронирована на " + date))
+                .shouldBe(visible, Duration.ofSeconds(15));
     }
 }
